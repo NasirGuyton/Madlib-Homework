92e04248ff8cf7f22e365efd3c99b3b726b64790
NasirGuyton <nasirjoseph.guyton@students.dominican.edu>
2024-09-09 21:34:02 -0700

    Add files via upload

---
+++ madlib homeowork.py
@@ -0,0 +1,66 @@
+
+# Homework: Create a madlib. Imagine a story where some of the words are 
+# supplied by user input. Using python you will use input to collect 
+# words for a story and then display the story. 
+
+# Use input to collect each word to a variable
+# Use an f string to display the story
+
+# Your madlib must collect at least 6 words!
+
+#a madlib ill use is peter piper picked a peck of pickled peppers 
+
+#Peter Piper picked a peck of pickled peppers, A peck of pickled peppers
+#Peter Piper picked; If Peter Piper picked a peck of pickled peppers,
+#Wheres the peck of pickled peppers Peter Piper picked?
+
+Peter = " Peter Piper"
+Pickled = " pickled peppers"
+A = " a peck"
+Picked = " picked a peck"
+where = " Wheres the"
+peck = " peck of"
+
+line1 = Peter + Picked + Pickled + ", A" + peck + Pickled
+line2 = Peter + " Picked; if " + Peter + Picked + "of" + Pickled + ","
+line3 = where + peck + Pickled + Peter + " picked?"
+
+print (line1)
+print (line2)
+print (line3)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
